262bb31afd39d8d4fe9100c432d4ff40205f1bdb
lailaeliotti1 <eliottilaila@gmail.com>
2022-06-03 11:28:54 -0700

    Initial commit, Flixster app before stretch features

---
+++ app/src/main/java/com/example/flixster2/Movie.java
@@ -0,0 +1,55 @@
+package com.example.flixster2;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class Movie {
+    String posterPath;
+    String backdropPath;
+    String title;
+    String overview;
+    Double vote_average;
+    Double popularity;
+
+    public Movie(){}
+
+
+    public Movie(JSONObject jsonObject) throws JSONException{
+         posterPath = jsonObject.getString("poster_path");
+         backdropPath = jsonObject.getString("backdrop_path");
+         title = jsonObject.getString("title");
+         overview = jsonObject.optString("overview");
+         vote_average = jsonObject.getDouble("vote_average");
+         popularity = jsonObject.getDouble("popularity");
+
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+         List<Movie> movies = new ArrayList<>();
+         for(int i = 0; i < movieJsonArray.length(); i++) {
+             movies.add(new Movie(movieJsonArray.getJSONObject(i))) ;
+         }
+         return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s" , posterPath); //https://image.tmbd.org/t/p[IMAGE_SIZE]
+    }                      //"https://image.tmdb.org/t/p/w342/%s"
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s" , backdropPath); //https://image.tmbd.org/t/p[IMAGE_SIZE]
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getOverview() {
+        return overview;
+    }
+    public Double getVoteAverage() { return vote_average; }
+    public Double getPopularity() { return popularity;}
+}
