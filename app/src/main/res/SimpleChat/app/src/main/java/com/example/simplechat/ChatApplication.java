ac450b40b52e1d0e6457b3f11a00b9c5865d4a41
lailaeliotti1 <eliottilaila@gmail.com>
2022-06-13 13:52:08 -0700

    step 0

--- app/src/main/res/SimpleChat/app/src/main/java/com/example/simplechat/ChatApplication.java
+++ app/src/main/res/SimpleChat/app/src/main/java/com/example/simplechat/ChatApplication.java
@@ -3,14 +3,18 @@ package com.example.simplechat;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ChatApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(Message.class);
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("BgMeRhXN4Mfsr4iu7cg2PJPITPJgn9T4meopyWPG") // provided in Lab instructions
                 .clientKey("r6fZMD0BL8dZx0pVlYemVJbHwqaBtIRqO19SX40c") // provided in Lab instructions
